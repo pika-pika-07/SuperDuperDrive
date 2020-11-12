2546d17231d66126e1febac45a66d93b5b3d537d
Bhatnagar <parth@BZ01493.bbrouter>
2020-11-12 22:51:21 +0530

    Initail Commit

---
+++ src/test/java/com/example/SuperDuperDrive/SuperDuperDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SuperDuperDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDuperDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
