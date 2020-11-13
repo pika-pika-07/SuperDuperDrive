779a3bfd958592da956ffe41d69976194588b5ff
Bhatnagar <parth@BZ01493.bbrouter>
2020-11-13 19:48:35 +0530

    Added user mapper,security config, user model, authentcation serice

---
+++ src/main/java/com/example/SuperDuperDrive/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.SuperDuperDrive.mapper;
+
+import com.example.SuperDuperDrive.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Select("SELECT * FROM USERS WHERE username = #{username}")
+    User getUser(String username);
+
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname)"
+            + " VALUES(#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
+    @Options(useGeneratedKeys = true, keyProperty = "userid")
+    Integer insert(User user);
+}
