97298900bb686a12bc8907942fd7c9aad283a97d
Bhatnagar <parth@BZ01493.bbrouter>
2020-11-29 01:49:39 +0530

    Added signup html:

--- src/main/java/com/example/SuperDuperDrive/controller/LoginController.java
+++ src/main/java/com/example/SuperDuperDrive/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.example.SuperDuperDrive.mapper;
+package com.example.SuperDuperDrive.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
