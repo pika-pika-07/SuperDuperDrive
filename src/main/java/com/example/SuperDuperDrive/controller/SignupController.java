8e0db06935af4f4aa8b39803a862fa613f4338ae
Bhatnagar <parth@MacBook-Pro.bbrouter>
2020-11-30 04:01:02 +0530

    Added signup functionality

--- src/main/java/com/example/SuperDuperDrive/controller/SignupController.java
+++ src/main/java/com/example/SuperDuperDrive/controller/SignupController.java
@@ -1,15 +1,43 @@
 package com.example.SuperDuperDrive.controller;
 
+import com.example.SuperDuperDrive.model.User;
+import com.example.SuperDuperDrive.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
-    @GetMapping
-    public String signup(){
+    private final UserService userService;
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping()
+    public String signup(@ModelAttribute User user, Model model ){
         return "signup";
+    }
+
+    @PostMapping()
+    public String userSignup(@ModelAttribute User user, Model model) {
+        model.addAttribute("user",new User());
+        String error = null;
+        if(userService.isUserNameTaken(user.getUsername())){
+            error = "UserName Not Available";
+            model.addAttribute("signupError", error);
+        }
+        else{
+            int result = userService.signupUser(user);
+            System.out.println(result);
+        }
+
+        return "signup";
+
     }
 
 }
---
+++ src/main/java/com/example/SuperDuperDrive/services/UserService.java
@@ -0,0 +1,41 @@
+package com.example.SuperDuperDrive.services;
+
+import com.example.SuperDuperDrive.mapper.UserMapper;
+import com.example.SuperDuperDrive.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUserNameTaken(String username) {
+        return userMapper.getUser(username) != null;
+    }
+
+    public String Encoder(){
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        return Base64.getEncoder().encodeToString(key);
+    }
+
+    public int signupUser(User user) {
+        String encodedSalt = Encoder();
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        user.setSalt(encodedSalt);
+        user.setPassword(hashedPassword);
+        return userMapper.insert(user);
+    }
+
+}
